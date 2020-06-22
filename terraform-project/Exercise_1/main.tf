8259b1fa072c038d20ad03d5edd9645a91256aa5
Sabeer <sabeer mz>
2020-06-22 08:12:57 +0530

    fixed terraform and uploaded UdacityCloudTrailLog.csv

--- terraform-project/Exercise_1/main.tf
+++ terraform-project/Exercise_1/main.tf
@@ -16,9 +16,25 @@ module "ec2_cluster_T2" {
   subnet_id              = "subnet-038b6599cc5cb58e8"
 
   tags = {
-    Terraform   = "true"
+    Name = "Udacity T2"
   }
 }
 
+module "ec2_cluster_M4" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
 
+  name                   = "Udacity M4"
+  instance_count         = 2
+
+  ami                    = "ami-0c6b1d09930fac512"
+  instance_type          = "m4.large"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0f7c99a36a9020d85"]
+  subnet_id              = "subnet-038b6599cc5cb58e8"
+
+  tags = {
+    Name = "Udacity M4"
+  }
+}
 
